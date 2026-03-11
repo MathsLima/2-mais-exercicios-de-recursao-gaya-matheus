9186b95c4056810445efaa75c03d3420955e2696
Matheus Lima <75711232+MathsLima@users.noreply.github.com>
2026-03-11 20:26:50 -0300

    ex3

---
+++ exercicio3.java
@@ -0,0 +1,14 @@
+public class exercicio3 {
+    public static int fibo(int n){
+	if (n <= 0)
+		return 0;
+	if (n==1)
+		return 1;
+	return fibo(n-1) + fibo(n-2);
+    }
+
+    public static void main(String[] args) {
+        int num = 3;
+        System.out.println("Fibonnaci: " + fibo(num));
+    }
+}
